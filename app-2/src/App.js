ded83cc2ad6ec22daa08c133d998c54e63d144f8
sunnyko3 <57108786+sunnyko3@users.noreply.github.com>
2019-12-10 16:21:07 -0700

    turnin

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -3,18 +3,20 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      foods: ["spaghetti", "ice cream", "sushi", "bologna", "cheese", "ham"]
+    };
+  }
+
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    let foodsToDisplay = this.state.foods.map((element, index) => {
+      return <h2 key={index}>{element}</h2>;
+    });
+
+    return <div className="App">{foodsToDisplay}</div>;
   }
 }
 
